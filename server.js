9709d070babf3ded840c25c51cb188c7d42bb782
Greg <cryptomonger@hotmail.com>
2016-12-10 09:04:09 +0000

    added nodemon added to mocha and chai

--- server.js
+++ server.js
@@ -18,13 +18,40 @@ app.use(express.static("public"));
 
 app.get('/', (req, res)=>{res.sendFile(process.cwd() + '/public/layout.html')});
 
-
-
 // Command to run Server: nodemon ./server.js
 
 http.createServer(app).listen(process.env.PORT, process.env.IP);
 
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
